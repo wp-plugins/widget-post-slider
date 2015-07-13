67726c28f8b7a2dab302489ac45b42dd18f61cee
shapedtheme <shapedtheme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 22:57:23 +0000

    First version released
    git-svn-id: https://plugins.svn.wordpress.org/widget-post-slider/trunk@1198379 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+﻿jQuery(function($){
+
+'use strict';
+
+
+  // -------------------------------------------------------------
+  // Flex Slider
+  // -------------------------------------------------------------
+
+    (function () {
+    $(document).ready(function() {
+                $('.carousel').carousel()
+                $(".carousel-inner .item:first").addClass("active");
+            });
+    }());
+
+	
+});
