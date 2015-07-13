c7fecb9f094bf0891c53f1bdacce8d7f759b3631
shapedtheme <shapedtheme@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 22:57:23 +0000

    First version released
    git-svn-id: https://plugins.svn.wordpress.org/widget-post-slider/tags/1.0@1198379 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Widget Post Slider ===
+Contributors: rubel_miah, shapedtheme, shamimmiashuhagh
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=ashidmiah@gmail.com&lc=US&item_name=Donate&currency_code=USD&bn=PP-DonationsBF:btn_donate_SM.gif:NonHostedGuest
+Tags: widget post slider, latest posts widget slider, widget, posts widget, category posts slider, latest category posts slider, post thumbnails, slider widget, widget slider post
+Requires at least: 4+
+Tested up to: 4.2.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Widget Post Slider to display posts from category.
+
+
+== Description ==
+
+
+
+== Installation ==
+
+1. Download the plugin.
+2. Upload it to the plugins folder of your blog.
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Now, goto the Widgets page of the Appearance section and configure the Widget Post Slider.
+
+
+
+== Upgrade Notice ==
+Nothing here!
+
+
+
+== Screenshots ==
+
+1. Live Demo.
+2. Widget area.
+
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial Release
---
+++ widget-post-slider.php
@@ -0,0 +1,180 @@
+<?php
+/*
+Plugin Name: Widget Post Slider
+Plugin URI: http://shapedtheme.com/demo/plugins/widget-post-slider
+Author: ShapedTheme
+Author URI: http://shapedtheme.com
+Description: Widget Post Slider to display posts from category.
+Version: 1.0
+License: GPL2
+Text Domain: shaped_theme
+*/
+
+
+// Don't call the file directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+define('widget_post_slider_plugin_url', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+
+
+function st_widget_post_slider_init_scripts()
+    {
+    	//CSS
+    	wp_enqueue_style('wps_style', widget_post_slider_plugin_url. 'css/style.css' );
+    	wp_enqueue_style('owl_carousel_css', widget_post_slider_plugin_url. 'css/bootstrap.min.css');
+        
+        //JS
+        wp_enqueue_script( 'jquery' );
+        wp_enqueue_script('owl_carousel_js', plugins_url('/js/bootstrap.min.js', __FILE__), array ('jquery'));
+        wp_enqueue_script('scripts_js', plugins_url('/js/scripts.js', __FILE__), array('jquery'));
+
+    }
+add_action("init","st_widget_post_slider_init_scripts");
+
+
+//Thumbnail Size
+add_image_size('wps_thumbnail_size', 300, 230, TRUE);
+
+
+//wadget
+add_action('widgets_init','register_shapedtheme_widget_post_slider');
+
+function register_shapedtheme_widget_post_slider()
+{
+	register_widget('ST_Widget_Post_Slider');
+}
+
+class ST_Widget_Post_Slider extends WP_Widget{
+
+	function ST_Widget_Post_Slider()
+	{
+		$this->WP_Widget( 'ST_Widget_Post_Slider', __('Widget Post Slider', 'shaped_theme') , array('description' => __('Widget Post Slider to display posts', 'shaped_theme') ) );
+	}
+
+
+	/*-------------------------------------------------------
+	 *				Front-end display of widget
+	 *-------------------------------------------------------*/
+
+	function widget($args, $instance)
+	{
+		extract($args);
+
+		$title 			= apply_filters('widget_title', $instance['title'] );
+		$count 			= $instance['count'];
+		$cat_ID 		= $instance['cat_name'];
+		
+		echo $before_widget;
+
+		$output = '';
+
+		if ( $title )
+			echo $before_title . $title . $after_title;
+
+		global $post;
+
+
+		$args = array( 
+			'posts_per_page' 	=> $count,
+			'category'			=> $cat_ID
+		);
+
+		$posts = get_posts( $args );
+
+		if(count($posts)>0){
+			$output .='<div id="gallery-carousel" class="carousel slide" data-ride="carousel">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">';
+
+			foreach ($posts as $post): setup_postdata($post);
+				$output .='<div class="item">';
+
+					if(has_post_thumbnail()):
+						$output .='<div class="post-slider">';
+						$output .='<a href="'.get_permalink().'">'.get_the_post_thumbnail($post->ID, 'wps_thumbnail_size', array('class' => 'img-responsive')).'</a>';
+						$output .= '<div class="carousel-caption"><p><a href="'.get_permalink().'">'. get_the_title() .'.</a></p></div>';
+						$output .='</div>';
+					endif;
+
+					
+
+
+				$output .='</div>';
+			endforeach;
+
+			wp_reset_query();
+
+			$output .=' </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#gallery-carousel" role="button" data-slide="prev">
+                  <i class="glyphicon glyphicon-menu-left"></i>
+                </a>
+                <a class="right carousel-control" href="#gallery-carousel" role="button" data-slide="next">
+                  <i class="glyphicon glyphicon-menu-right"></i>
+                </a>
+              </div>';
+		}
+
+
+		echo $output;
+
+		echo $after_widget;
+	}
+
+
+	function update( $new_instance, $old_instance )
+	{
+		$instance = $old_instance;
+
+		$instance['title'] 			= strip_tags( $new_instance['title'] );
+		$instance['cat_name'] 		= strip_tags( $new_instance['cat_name'] );
+		$instance['count'] 			= strip_tags( $new_instance['count'] );
+
+		return $instance;
+	}
+
+
+	function form($instance)
+	{
+		$defaults = array( 
+			'title' 	=> 'Widget Post Slider',
+			'cat_name' 	=> ' ',
+			'width' 	=> '300',
+			'count' 	=> 5
+		);
+		$instance = wp_parse_args( (array) $instance, $defaults );
+	?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Widget Title:</label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'cat_name' ); ?>">Select Category</label>
+			<?php 
+				$categories = get_categories(array('hierarchical' => false));
+				if(isset($instance['cat_name'])) $cat_ID = $instance['cat_name'];
+			?>
+			<select class="widefat" id="<?php echo $this->get_field_id( 'cat_name' ); ?>" name="<?php echo $this->get_field_name( 'cat_name' ); ?>">
+
+
+			<option value='all' <?php if ('all' == $instance['cat_name']) echo 'selected="selected"'; ?>><?php _e('All categories', 'shaped_theme') ?></option>
+			<?php $categories = get_categories('hide_empty=0&depth=1&type=post'); ?>
+			<?php foreach($categories as $category) { ?>
+			<option value='<?php echo $category->term_id; ?>' <?php if ($category->term_id == $instance['cat_name']) echo 'selected="selected"'; ?>><?php echo $category->cat_name; ?></option>
+			<?php } ?>
+
+
+
+			</select>
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'count' ); ?>"><?php _e('Slide Count', 'shaped_theme') ?></label>
+			<input id="<?php echo $this->get_field_id( 'count' ); ?>" name="<?php echo $this->get_field_name( 'count' ); ?>" value="<?php echo $instance['count']; ?>" style="width:100%;" />
+		</p>
+
+	<?php
+	}
+}
